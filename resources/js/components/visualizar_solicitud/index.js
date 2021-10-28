258bccdc0c1bf0b6d91dc287fd6e27e6e72db37a
ManuelAlejandro20 <mtrigo@adom.cl>
2021-10-28 00:22:20 -0300

    feat: tabla de archivos añadida

--- resources/js/components/visualizar_solicitud/index.js
+++ resources/js/components/visualizar_solicitud/index.js
@@ -233,27 +233,23 @@ function VisualizarSolicitud() {
             </Grid>         
             <Grid item>
                 <MaterialTable
-                    title="One Detail Panel Preview"
+                    title="Archivos adjuntos"
+                    options={{search: false, paging: false}}           
                     columns={[
-                        { title: 'Name', field: 'name' },
-                        { title: 'Surname', field: 'surname' },
-                        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-                        {
-                        title: 'Birth Place',
-                        field: 'birthCity',
-                        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-                        },
+                        { title: 'Nombre archivo', field: 'nombreArchivo' },
+                        { title: 'Peso', field: 'peso' },
+                        { title: 'Persona correspondiente', field: 'Persona'},
                     ]}
                     data={[
-                        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-                        { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+                        { name: 'Mehmet', surname: 'Baran', birthYear: "1987"},
+                        { name: 'Zerya Betül', surname: 'Baran', birthYear: "2017"},
                     ]}
                     detailPanel={rowData => {
                         return (
                         <iframe
                             width="100%"
-                            height="315"
-                            src="https://www.youtube.com/embed/C0DPdy98e4c"
+                            height="1120"
+                            src="https://fi.ort.edu.uy/innovaportal/file/2021/1/metodologia_xp.pdf"
                             frameborder="0"
                             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen
