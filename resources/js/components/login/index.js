b6dfc5528a228578ad486945799cde5d7bf53029
ManuelAlejandro20 <mtrigo@adom.cl>
2021-10-28 23:25:56 -0300

    feat: botón cerrar sesión añadido en perfil

--- resources/js/components/login/index.js
+++ resources/js/components/login/index.js
@@ -1,16 +1,11 @@
 import { React, useState } from 'react';
 import {Grid, Paper, Avatar, TextField, Typography, Button, Divider} from '@material-ui/core'
-//import { Button } from '@mui/material';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
 function Login() {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [rut, setRut] = useState("");
-    const [phone, setPhone] = useState("");
-    const [hasAccount, setHasAccount] = useState(true);
   
     const paperStyle = {
       padding: 20,
@@ -19,14 +14,7 @@ function Login() {
       margin: "20px auto",
       border: "1px solid #003057",
     };
-  
-    const paperStyleLarge = {
-      padding: 20,
-      height: "85vh",
-      width: 400,
-      margin: "20px auto",
-    };
-  
+    
     const avatarStyle = { backgroundColor: "#003057" };
         
     return (
@@ -42,15 +30,15 @@ function Login() {
           <Grid item>
             <form>
                 <Grid>
-                  <Paper elevation={0} style={hasAccount ? paperStyle : paperStyleLarge}>
-                    <Grid align="center" container direction={"column"} spacing={3}>
+                  <Paper elevation={0} style={paperStyle}>
+                    <Grid align="center" container direction={"column"} spacing={4}>
                       <Grid item>
                         <Avatar style={avatarStyle}>
                           <LockOutlinedIcon />
                         </Avatar>
                       </Grid>
                       <Grid item>
-                        <h4>{hasAccount? 'Iniciar sesión' : 'Regístrate'}</h4>
+                        <h4>Iniciar sesión</h4>
                       </Grid>                      
                     </Grid>
                     <Grid container direction={"column"} spacing={2}>
@@ -68,56 +56,21 @@ function Login() {
                           autoFocus
                         />
                       </Grid>
-                      {hasAccount?
-                        <Grid item>
-                          <TextField
-                            id="password"
-                            name="password"
-                            label="Contraseña"
-                            placeholder="Ingresa tu contraseña"
-                            variant="outlined"
-                            type="password"
-                            value={password}
-                            onChange={(e) => {setPassword(e.target.value);}}
-                            fullWidth
-                            required
-                            autoFocus
-                          />
-                        </Grid>
-                          : (
-                        <>
-                        <Grid item>
-                          <TextField
-                            id="name"
-                            name="name"
-                            label="Nombre"
-                            placeholder="Ingresa tu nombre completo"
-                            variant="outlined"
-                            type="text"
-                            value={name}
-                            onChange={(e) => {setName(e.target.value);}}
-                            fullWidth
-                            required
-                            autoFocus
-                          />
-                        </Grid>
-                        <Grid item>
-                          <TextField
-                            id="rut"
-                            name="rut"
-                            label="Rut"
-                            placeholder="Ingresa tu rut (incluya puntuación y guión)"
-                            variant="outlined"
-                            type="text"
-                            value={rut}
-                            onChange={(e) => {setRut(e.target.value);}}
-                            fullWidth
-                            required
-                            autoFocus
-                          />
-                        </Grid>                 
-                        </>
-                        )}
+                      <Grid item>
+                        <TextField
+                          id="password"
+                          name="password"
+                          label="Contraseña"
+                          placeholder="Ingresa tu contraseña"
+                          variant="outlined"
+                          type="password"
+                          value={password}
+                          onChange={(e) => {setPassword(e.target.value);}}
+                          fullWidth
+                          required
+                          autoFocus
+                        />
+                      </Grid>
                       <Grid item align="center">
                         <Button
                           className="linkItem"
@@ -127,14 +80,8 @@ function Login() {
                           href="/home"
                         //   onClick={hasAccount ? handleLogin : handleSignup}
                         >
-                          {hasAccount? 'Iniciar sesión' : 'Registrarse'}
+                          Iniciar sesión
                         </Button>
-                        <p>
-                        {hasAccount? '¿No tienes cuenta?' : '¿Ya tienes una cuenta?'}
-                        <span className="loginMsg" 
-                              // onClick={() => setHasAccount(!hasAccount)}
-                              >{hasAccount? '   Regístrate' : ' Inicia sesión'}</span>              
-                        </p>
                       </Grid>
                     </Grid>
                   </Paper>
--- resources/js/components/menucontainer/index.js
+++ resources/js/components/menucontainer/index.js
@@ -2,6 +2,7 @@
 /** @jsx jsx */
 import { css, jsx, Global } from "@emotion/react";
 import {Grid, Typography, Divider} from '@material-ui/core'
+import { Button } from '@material-ui/core'
 import MenuItems from "../menuitems";
 
 function MenuContainer() {
@@ -13,12 +14,28 @@ function MenuContainer() {
         height: 100%;
       `}
     >
-        <Grid container direction={"column"} spacing={3}>
+        <Grid container direction={"column"} spacing={1}>
             <Grid item>
-            <Typography variant="h4" gutterBottom style={{ color: "#003057" }}>
-                Perfil estudiante
-            </Typography>
-            <Divider light />
+              <Grid container direction={"column"} spacing={3}>
+                <Grid item>
+                  <Typography variant="h4" gutterBottom style={{ color: "#003057" }}>
+                      Perfil estudiante
+                  </Typography>                            
+                  <Divider light />        
+                </Grid>
+                <Grid item>
+                  <Button
+                    className="linkItem"
+                    style={{maxWidth: '150px', minWidth: '150px', float:"right"}}
+                    variant="contained"
+                    color="primary"
+                    href="/"
+                  //   onClick={hasAccount ? handleLogin : handleSignup}
+                    >
+                    Cerrar sesión
+                  </Button>                        
+                </Grid>                            
+              </Grid>               
             </Grid>
             <Grid item>
                 <MenuItems/>
